d4393650139ed979006c0cc3a455998923cc710a
Hypotermya <migueljaimes10a@gmail.com>
2025-09-29 09:22:56 -0500

    Fix: ruta del modelo para Docker y actualización de Dockerfile

--- app/api.py
+++ app/api.py
@@ -3,14 +3,21 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
-model = joblib.load("C:/Documentos/heart-desease-mlops/model.joblib")
+# Cargar el modelo entrenado
+model = joblib.load("model.joblib")
 app = FastAPI()
+
 
 class Input(BaseModel):
     features: list
+
 
 @app.post("/predict")
 def predict(data: Input):
     X = np.array(data.features).reshape(1, -1)
     proba = model.predict_proba(X)[0][1]
-    return {"heart_disease_probability": proba, "prediction": int(proba > 0.5)}
+    return {
+        "heart_disease_probability": float(proba),
+        "prediction": int(proba > 0.5),
+    }
+
